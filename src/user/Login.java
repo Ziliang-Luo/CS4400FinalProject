a33bcbd6ae5845af8d0ad4fe9a48a129b6e79b01
Ziliang-Luo <38770746+Ziliang-Luo@users.noreply.github.com>
2019-04-16 17:36:26 -0500

    Update Login.java

--- src/user/Login.java
+++ src/user/Login.java
@@ -1,30 +1,79 @@
 package user;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-//import java.sql.*;
 
-public class Login extends HttpServlet{
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+//import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import db_connection.Db_connection;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
         PrintWriter out = response.getWriter();
-        
+        doGet(request, response);
+
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        
-        if(Validate.checkUser(email, password))
-        {
-            RequestDispatcher rs = request.getRequestDispatcher("UserRegister.jsp");
-            rs.forward(request, response);
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myConn = Db_connection.getConnection();
+            Statement stmt = myConn.createStatement();
+            ResultSet rs = stmt.executeQuery("select email,password from User where email = '"+email+"' and password = '"+password+"' ");
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("email", email);
+                response.sendRedirect("homePage.jsp");
+                out.println("correct email or password");
+            }else {
+                out.println("Wrong email or password");
+            }
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
-        
-        else
-        {
-           out.println("Username or Password incorrect");
-           RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
-           rs.include(request, response);
-        }
-    }  
+
+    }
+
 }
